0a6aafcf2a299ba247e65bc57e433a4c613be729
SiegfriedWagner <mateus.chojnowski@gmail.com>
2021-11-03 19:37:41 +0100

    fixed gradient queen

--- gradient_queen/main.cpp
+++ gradient_queen/main.cpp
@@ -13,10 +13,32 @@ void printSolution(const vector<uint_fast32_t> &rows) {
             if (rows[col] == row)
                 cout << "H ";
             else
-                cout << "O ";
+                cout << ". ";
         }
         cout << endl;
     }
+}
+
+uint_fast32_t calculateConflicts(vector<uint_fast32_t> &northEast, vector<uint_fast32_t> &northWest, vector<uint_fast32_t> &rows) {
+    uint_fast32_t numberOfConflicts = 0;
+    for (int i = 0; i < northEast.size(); ++i) {
+        northEast[i] = 0;
+        northWest[i] = 0;
+    }
+    for (uint_fast32_t row = 0; row < rows.size(); ++row) {
+        auto col = rows[row];
+        auto northWestIndex = col + rows.size() - row - 1;
+        auto northEastIndex = col + row;
+        northWest[northWestIndex]++;
+        northEast[northEastIndex]++;
+    }
+    for (int i = 0; i < northEast.size(); ++i) {
+        if (northEast[i] > 0)
+            numberOfConflicts += northEast[i] - 1;
+        if (northWest[i] > 0)
+            numberOfConflicts += northWest[i] - 1;
+    }
+    return numberOfConflicts;
 }
 
 vector<uint_fast32_t> placeQueens(uint_fast32_t chessboardSize) {
@@ -28,78 +50,28 @@ vector<uint_fast32_t> placeQueens(uint_fast32_t chessboardSize) {
     // mix initial positions
     uint_fast32_t numberOfConflicts = 1;
     while(numberOfConflicts > 0) {
-        numberOfConflicts = 0;
         shuffle(rows.begin(), rows.end(), std::mt19937(std::random_device()()));
-        // zero conflict vector
-        for (int i = 0; i < northEast.size(); ++i) {
-            northEast[i] = 0;
-            northWest[i] = 0;
-        }
-        for (uint_fast32_t row = 0; row < rows.size(); ++row) {
-            auto col = rows[row];
-            auto northWestIndex = col + rows.size() - row - 1;
-            auto northEastIndex = col + row;
-            if (++northWest[northWestIndex] > 1)
-                numberOfConflicts+=2;
-            if (++northEast[northEastIndex] > 1)
-                numberOfConflicts+=2;
-        }
-        // cout << "Number of conflicts: " << numberOfConflicts << endl;
+        numberOfConflicts = calculateConflicts(northEast, northWest, rows);
         uint_fast32_t swaps = 1;
         while (numberOfConflicts > 0 && swaps > 0) {
             swaps = 0;
             for (int icol = 0; icol < rows.size(); ++icol) {
                 for (int jcol = icol + 1; jcol < rows.size(); ++jcol) {
-                    auto conflictsCopy = numberOfConflicts;
-                    auto irow = rows[icol];
-                    auto inorthWestIndex = icol + rows.size() - irow - 1;
-                    auto inorthEastIndex = icol + irow;
-                    auto jrow = rows[jcol];
-                    auto jnorthWestIndex = jcol + rows.size() - jrow - 1;
-                    auto jnorthEastIndex = jcol + jrow;
-                    auto inewNorthWestIndex = jcol + rows.size() - irow - 1;
-                    auto inewNorthEastIndex = jcol + irow;
-                    auto jnewNortWestIndex = icol + rows.size() - jrow - 1;
-                    auto jnewNorthEastIndex = icol + jrow;
-                    if (northWest[inorthWestIndex]-- > 1)
-                        conflictsCopy--;
-                    if (northEast[inorthEastIndex]-- > 1)
-                        conflictsCopy--;
-                    if (northWest[jnorthWestIndex]-- > 1)
-                        conflictsCopy--;
-                    if (northEast[jnorthEastIndex]-- > 1)
-                        conflictsCopy--;
-                    if (++northWest[inewNorthWestIndex] > 1)
-                        conflictsCopy++;
-                    if (++northEast[inewNorthEastIndex] > 1)
-                        conflictsCopy++;
-                    if (++northWest[jnewNortWestIndex] > 1)
-                        conflictsCopy++;
-                    if (++northEast[jnewNorthEastIndex] > 1)
-                        conflictsCopy++;
+                    swap(rows[icol], rows[jcol]);
+                    auto newConflicts = calculateConflicts(northEast, northWest, rows);
                     // check conflicts number
-                    if (conflictsCopy < numberOfConflicts) {
+                    if (newConflicts < numberOfConflicts) {
                         // accept change
-                        swap(rows[icol], rows[jcol]);
-                        // cout << "New conflicts :" << conflictsCopy << endl;
-                        numberOfConflicts = conflictsCopy;
+                        numberOfConflicts = newConflicts;
                         swaps++;
                     }
                     else {
                         // rollback
-                        ++northWest[inorthWestIndex];
-                        ++northEast[inorthEastIndex];
-                        ++northWest[jnorthWestIndex];
-                        ++northEast[jnorthEastIndex];
-                        --northWest[inewNorthWestIndex];
-                        --northEast[inewNorthEastIndex];
-                        --northWest[jnewNortWestIndex];
-                        --northEast[jnewNorthEastIndex];
+                        swap(rows[icol], rows[jcol]);
                     }
 
                 }
             }
-            // cout << "Swaps: " << swaps << endl;
         }
     }
     assert(numberOfConflicts == 0);
